865506f41c551a6c86924735a22c6360c98665cb
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-11-30 15:37:34 -0500

    spring

---
+++ src/main/java/com/nathan/memex/MemexApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.memex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathan/memex/MemexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.memex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
