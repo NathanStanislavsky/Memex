1cdc5a0f55ded786a6600dc3e74be94097319727
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-12-01 18:04:17 -0500

    MemexRepository

---
+++ src/main/java/com/nathan/memex/MemexRepository.java
@@ -0,0 +1,8 @@
+package com.nathan.memex;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemexRepository extends ElasticsearchRepository<MemexDocument, String> {
+}
