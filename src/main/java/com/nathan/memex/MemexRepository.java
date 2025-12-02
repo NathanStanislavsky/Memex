7ef578bafa2eededaf3d836fb79a0716625c7ee0
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-12-01 21:18:04 -0500

    fuzzy matching for search

--- src/main/java/com/nathan/memex/MemexRepository.java
+++ src/main/java/com/nathan/memex/MemexRepository.java
@@ -1,10 +1,12 @@
 package com.nathan.memex;
 
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface MemexRepository extends ElasticsearchRepository<MemexDocument, String> {
+    @Query("{\"match\": {\"content\": {\"query\": \"?0\", \"fuzziness\": \"AUTO\"}}}")
     List<MemexDocument> findByContent(String text);
 }
