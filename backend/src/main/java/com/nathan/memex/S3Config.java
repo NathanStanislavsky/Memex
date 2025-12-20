4a7cdb3213264ed0b0056c327c691abdebc9ab99
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-12-19 22:17:46 -0500

    updated s3config

--- backend/src/main/java/com/nathan/memex/S3Config.java
+++ backend/src/main/java/com/nathan/memex/S3Config.java
@@ -1,5 +1,6 @@
 package com.nathan.memex;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -12,13 +13,22 @@ import java.net.URI;
 @Configuration
 public class S3Config {
 
+    @Value("${S3_ENDPOINT:http://localhost:9000}")
+    private String s3Endpoint;
+
+    @Value("${S3_ACCESS_KEY:admin}")
+    private String s3AccessKey;
+
+    @Value("${S3_SECRET_KEY:password}")
+    private String s3SecretKey;
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.US_EAST_1)
-                .endpointOverride(URI.create("http://localhost:9000"))
+                .endpointOverride(URI.create(s3Endpoint))
                 .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("admin", "password")
+                        AwsBasicCredentials.create(s3AccessKey, s3SecretKey)
                 ))
                 .forcePathStyle(true)
                 .build();
