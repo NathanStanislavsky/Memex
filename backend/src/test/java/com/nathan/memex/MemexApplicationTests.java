5ce37f36134632ccd7947951736ab1ee01f69c42
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-12-21 16:13:09 -0500

    mock db connections

--- backend/src/test/java/com/nathan/memex/MemexApplicationTests.java
+++ backend/src/test/java/com/nathan/memex/MemexApplicationTests.java
@@ -2,12 +2,25 @@ package com.nathan.memex;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 class MemexApplicationTests {
+    @MockBean
+    private ConnectionFactory connectionFactory;
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+    @MockBean
+    private S3Client s3Client;
+
+    @Test
+    void contextLoads() {
+
+    }
 
 }
