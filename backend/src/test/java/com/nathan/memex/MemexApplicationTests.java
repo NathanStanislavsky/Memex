52c92e6fbca499aa2bb0677e8729d11d002a4a0e
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-12-21 16:16:54 -0500

    fix version compatibility

--- backend/src/test/java/com/nathan/memex/MemexApplicationTests.java
+++ backend/src/test/java/com/nathan/memex/MemexApplicationTests.java
@@ -4,23 +4,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import org.elasticsearch.client.RestHighLevelClient;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 class MemexApplicationTests {
+
+    // 1. Mock RabbitMQ
     @MockBean
     private ConnectionFactory connectionFactory;
 
+    // 2. Mock Elasticsearch
     @MockBean
-    private ElasticsearchTemplate elasticsearchTemplate;
+    private RestHighLevelClient restHighLevelClient;
 
+    // 3. Mock S3 Client (MinIO)
     @MockBean
     private S3Client s3Client;
 
     @Test
     void contextLoads() {
-
+		
     }
 
 }
