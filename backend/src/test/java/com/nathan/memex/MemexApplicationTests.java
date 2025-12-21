9b2b689f52501c859d09a8f72f5a464409400491
NathanStanislavsky <nathan.stanislavsky16@gmail.com>
2025-12-21 16:21:57 -0500

    Fix ES client mock import

--- backend/src/test/java/com/nathan/memex/MemexApplicationTests.java
+++ backend/src/test/java/com/nathan/memex/MemexApplicationTests.java
@@ -4,27 +4,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.elasticsearch.client.RestHighLevelClient;
-import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 class MemexApplicationTests {
 
-    // 1. Mock RabbitMQ
-    @MockBean
-    private ConnectionFactory connectionFactory;
+	// 1. Mock RabbitMQ
+	@MockBean
+	private ConnectionFactory connectionFactory;
 
-    // 2. Mock Elasticsearch
-    @MockBean
-    private RestHighLevelClient restHighLevelClient;
+	// 2. Mock Elasticsearch
+	@MockBean(name = "elasticsearchClient")
+	private Object elasticsearchClient;
 
-    // 3. Mock S3 Client (MinIO)
-    @MockBean
-    private S3Client s3Client;
+	// 3. Mock S3 Client
+	@MockBean(name = "s3Client")
+	private Object s3Client;
 
-    @Test
-    void contextLoads() {
+	@Test
+	void contextLoads() {
 		
-    }
+	}
 
 }
